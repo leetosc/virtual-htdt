e3bb92d5e361f19d0da4a501cca686ca06a81067
Denny <leetosc@hotmail.com>
2021-01-30 02:33:02 -0600

    metadata

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,32 @@ export default function Home() {
     >
       <Head>
         <title>Virtual HTDT</title>
+        <meta name="title" content="DB Virtual HTDT" />
+        <meta
+          name="description"
+          content="Virtual Hành Trình Đức Tin for Đoàn Don Bosco Austin Virtual Camp 2021"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://virtualhtdt.ga" />
+        <meta property="og:title" content="DB Virtual HTDT" />
+        <meta
+          property="og:description"
+          content="Virtual Hành Trình Đức Tin for Đoàn Don Bosco Austin Virtual Camp 2021"
+        />
+        <meta property="og:image" content="https://virtualhtdt.ga/dblogo.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://virtualhtdt.ga/" />
+        <meta property="twitter:title" content="DB Virtual HTDT" />
+        <meta
+          property="twitter:description"
+          content="Virtual Hành Trình Đức Tin for Đoàn Don Bosco Austin Virtual Camp 2021"
+        />
+        <meta
+          property="twitter:image"
+          content="https://virtualhtdt.ga/dblogo.png"
+        />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box mx="auto" textAlign="center">
