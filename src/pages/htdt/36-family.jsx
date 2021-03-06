2efef218ea9aacd1189c28fc0701b9e8b918cc4f
Denny <leetosc@hotmail.com>
2021-03-06 13:44:06 -0600

    family

--- src/pages/htdt/36-family.jsx
+++ src/pages/htdt/36-family.jsx
@@ -55,45 +55,14 @@ export default function SaigonFamily() {
             <link rel="icon" href="/favicon.ico" />
           </Head>
 
-          {videoEnded ? (
-            <Box h="100%" position="relative" overflow="auto">
-              <Image src="/hue/taxi.png" maxW="unset" h="100%" maxH="100%" />
-            </Box>
-          ) : (
-            <Box h="100%" position="relative" overflow="auto" w="100%">
-              <Box
-                // overlay to prevent clicking video
-                opacity={0}
-                h="100%"
-                w="100%"
-                position="absolute"
-                top={0}
-                left={0}
-              />
-
-              <ReactPlayer
-                url="https://youtu.be/nVsRP3Pqesk"
-                controls={false}
-                muted={true}
-                playing={true}
-                playbackRate={1.25}
-                onProgress={({ playedSeconds }) => {
-                  if (playedSeconds > 205) {
-                    setVideoEnded(true);
-                  }
-                }}
-                config={{
-                  youtube: {
-                    playerVars: {
-                      start: 10,
-                    },
-                  },
-                }}
-                width="100%"
-                height="100%"
-              />
-            </Box>
-          )}
+          <Box h="100%" position="relative" overflow="auto">
+            <Image
+              src="/saigon/quanpho.jpg"
+              maxW="unset"
+              h="100%"
+              maxH="100%"
+            />
+          </Box>
         </Box>
 
         <Hud>
@@ -103,31 +72,45 @@ export default function SaigonFamily() {
               avgTypingDelay={5}
               onTypingDone={() => setTypingDone(true)}
             >
-              You arrive in Hue, the former imperial capital of Vietnam. You
-              decide to check out some of the city's popular attractions to see
-              if your uncle left any clues.
+              You family is happy to see you and take you out to get some
+              delicious local pho. They ask you about life in America, and you
+              ask them about life in Vietnam and about some of the holidays and
+              festivals that they like to celebrate.
             </Typist>
           </Box>
           <Box w="100%" pt={1}>
-            {appState.locationsVisited.includes("/htdt/31a-imperialcity") &&
-            appState.locationsVisited.includes("/htdt/31b-pagoda") &&
-            appState.locationsVisited.includes("/htdt/31c-dongba") ? (
-              <>
-                <Text>
-                  Taxi driver: &quot;It looks like you are heading to Saigon.
-                  I&apos;ll take you to the airport&quot;
-                </Text>
-                <Button
-                  mt={2}
-                  colorScheme="cyan"
-                  onClick={() => router.push("/htdt/31d-airport")}
-                >
-                  Go to airport
-                </Button>
-              </>
-            ) : (
-              <Text>Look for clues around the city</Text>
-            )}
+            <Stack>
+              <Text>
+                Your cousins each have a favorite festival. Click to learn more.
+              </Text>
+              <Button
+                colorScheme="cyan"
+                isDisabled={appState.locationsVisited.includes(
+                  "/htdt/31a-imperialcity"
+                )}
+                onClick={() => router.push("/htdt/31a-imperialcity")}
+              >
+                Ngay Nha Giao
+              </Button>
+              <Button
+                colorScheme="cyan"
+                isDisabled={appState.locationsVisited.includes(
+                  "/htdt/31b-pagoda"
+                )}
+                onClick={() => router.push("/htdt/31b-pagoda")}
+              >
+                Tet Trung Thu
+              </Button>
+              <Button
+                colorScheme="cyan"
+                isDisabled={appState.locationsVisited.includes(
+                  "/htdt/31c-dongba"
+                )}
+                onClick={() => router.push("/htdt/31c-dongba")}
+              >
+                Tet Nguyen Dan
+              </Button>
+            </Stack>
             {appState.SHOW_ANSWERS && (
               <Button
                 mt={2}
@@ -138,37 +121,7 @@ export default function SaigonFamily() {
               </Button>
             )}
           </Box>
-          <Box p={2}>
-            <Stack>
-              <Button
-                colorScheme="cyan"
-                isDisabled={appState.locationsVisited.includes(
-                  "/htdt/31a-imperialcity"
-                )}
-                onClick={() => router.push("/htdt/31a-imperialcity")}
-              >
-                Imperial City
-              </Button>
-              <Button
-                colorScheme="cyan"
-                isDisabled={appState.locationsVisited.includes(
-                  "/htdt/31b-pagoda"
-                )}
-                onClick={() => router.push("/htdt/31b-pagoda")}
-              >
-                Thien Mu Pagoda
-              </Button>
-              <Button
-                colorScheme="cyan"
-                isDisabled={appState.locationsVisited.includes(
-                  "/htdt/31c-dongba"
-                )}
-                onClick={() => router.push("/htdt/31c-dongba")}
-              >
-                Dong Ba Market
-              </Button>
-            </Stack>
-          </Box>
+          <Box p={2}></Box>
         </Hud>
       </GameLayout>
 
