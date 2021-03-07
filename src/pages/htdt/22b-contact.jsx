be76eb00c82f4cf51811b0fcb1891ed24a02bf50
Leeto <leetosc@hotmail.com>
2021-03-07 12:18:16 -0600

    lint fixes

--- src/pages/htdt/22b-contact.jsx
+++ src/pages/htdt/22b-contact.jsx
@@ -79,7 +79,7 @@ export default function HaLongContact() {
                 controls={false}
                 muted={true}
                 playing={true}
-                playbackRate={0.75}
+                playbackRate={1}
                 onEnded={() => {
                   setVideoEnded(true);
                 }}
@@ -113,7 +113,7 @@ export default function HaLongContact() {
             )}
           </Box>
           <Box w="100%" pt={1}>
-            <Text>Look for clues from your uncle.</Text>
+            <Text>Continue looking for clues.</Text>
           </Box>
           <Box p={2}>
             {modalOpened && (
